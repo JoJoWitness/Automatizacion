07819c83040f45c55de1ad2527da0a56d6f3942d
JoJoWitness <jordano470@gmail.com>
2024-12-08 10:36:50 -0400

    Added a loop cycle for the process and fixed the restart function

--- PLC-HMI/src/App.tsx
+++ PLC-HMI/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import './App.css'
 
@@ -12,6 +12,8 @@ function App() {
   const [process, setProcess] = useState(0)
   const [remainingTime, setRemainingTime] = useState(0)
 
+  const timeouts = useRef<number[]>([])
+
   useEffect(() => {
     let timer: number 
     if (remainingTime > 0) {
@@ -22,46 +24,57 @@ function App() {
     return () => clearInterval(timer)
   }, [remainingTime])
 
-  function run(){
+  function run() {
     setIsBreadMoving(true)
     setIsConveyorBeltLit1(true)
     setProcess(1)
     setRemainingTime(4)
-    setTimeout(() => {
+    timeouts.current.push(window.setTimeout(() => {
       setIsFurnaceOn(true)
       setIsConveyorBeltLit1(false)
       setProcess(2)
       setRemainingTime(10)
-      setTimeout(() => {
+      timeouts.current.push(window.setTimeout(() => {
         setIsFurnaceOn(false)
         setIsConveyorBeltLit2(true)
         setIsBreadBakedMoving(true)
         setProcess(3)
         setRemainingTime(4)
-        setTimeout(() => {
+        timeouts.current.push(window.setTimeout(() => {
           setIsConveyorBeltLit2(false)
           setProcess(4)
           setRemainingTime(0)
-          }, 4000)
-      }, 10000) // Furnace on for 10 seconds
-    }, 4000) // Bread moving for 4 seconds
+          timeouts.current.push(window.setTimeout(() => {
+            handleReset()
+            timeouts.current.push(window.setTimeout(() => {
+              run()
+            }, 1000))
+            
+          },1000))
+        }, 4000))
+      }, 10000))
+    }, 4000))
   }
 
-  function handleStartClick(){
-    if(isRunning == false){
+  function handleStart(){
+    if(!isRunning){
       run()
     }
    setIsRunning(true)
    
   }
 
-  const handleResetClick = () => {
+  const handleReset = () => {
+    setProcess(0)
+    setRemainingTime(0)
     setIsBreadMoving(false)
     setIsFurnaceOn(false)
     setIsBreadBakedMoving(false)
     setIsConveyorBeltLit1(false)
     setIsConveyorBeltLit2(false)
     setIsRunning(false)
+    timeouts.current.forEach(timeout => clearTimeout(timeout))
+    timeouts.current = []
   }
 
 let processArray=["Inactivo", "Cinta transpotadora 1", "Horneado","Cinta transpotadora 2", "Finalizado" ]
@@ -74,11 +87,11 @@ let processArray=["Inactivo", "Cinta transpotadora 1", "Horneado","Cinta transpo
             <div className='inputContainer'>
               <div className='buttomContainer'>
                 <h3>Iniciar</h3>
-                <button className='buttonStart' onClick={() => handleStartClick()}/>
+                <button className='buttonStart' onClick={() => handleStart()}/>
               </div>
               <div className='buttomContainer'>
                 <h3>Reset</h3>
-                <button className='buttonStop' onClick={() =>  handleResetClick()}/>
+                <button className='buttonStop' onClick={() =>  handleReset()}/>
               </div>
              
             </div>
@@ -103,7 +116,7 @@ let processArray=["Inactivo", "Cinta transpotadora 1", "Horneado","Cinta transpo
             </div>
           </div>
           <div className="nameContainer">
-              <p> Jordano Pernia, Automatización Industrial, seccion 1, 2024-3</p>
+              <p> Jordano Pernia, Automatización Industrial, sección 1, 2024-3</p>
            </div>
         </div>
         
